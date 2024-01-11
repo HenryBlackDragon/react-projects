6ad7f817aa2df64bee8efe924d7383a404e3a022
Alex Chaban <alex-chaban@mail.ru>
2024-01-11 19:17:30 +0300

    CS: add Footer

--- car-showcase/components/Hero.tsx
+++ car-showcase/components/Hero.tsx
@@ -1,16 +1,22 @@
 'use client'
 
 import Image from 'next/image'
-import CustomButton from './CustomButton'
+import { CustomButton } from '.'
 
 const Hero = () => {
-	const handleScroll = () => {}
+	const handleScroll = () => {
+		const nextSection = document.getElementById('discover')
+
+		if (nextSection) {
+			nextSection.scrollIntoView({ behavior: 'smooth' })
+		}
+	}
 
 	return (
 		<div className="hero">
 			<div className="flex-1 pt-36 padding-x">
 				<h1 className="hero__title">
-					Find, book or rent a car --quicly and easly!
+					Find, book, rent a car—quick and super easy!
 				</h1>
 
 				<p className="hero__subtitle">
@@ -24,14 +30,12 @@ const Hero = () => {
 					handleClick={handleScroll}
 				/>
 			</div>
-
 			<div className="hero__image-container">
 				<div className="hero__image">
 					<Image src="/hero.png" alt="hero" fill className="object-contain" />
 				</div>
+				<div className="hero__image-overlay" />
 			</div>
-
-			<div className="hero__image-overlay" />
 		</div>
 	)
 }
